76e50c39e3c3f1abd4c1394cc3e8d364384cd2b5
geleias-artesanais <billcorporations@gmail.com>
2024-08-28 11:54:22 -0300

    feat: inserido as contas do banco

---
+++ src/main/java/org/codetech/code/Conta.java
@@ -0,0 +1,42 @@
+package org.codetech.code;
+
+public class Conta{
+
+    int numero = 1021;
+    String agencia = "067-8";
+    String nomeCliente = "Pedro Nardelli";
+    double saldo = 237.48;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
--- src/main/java/org/codetech/code/ContaTerminal.java
+++ src/main/java/org/codetech/code/ContaTerminal.java
@@ -1,5 +1,7 @@
 package org.codetech.code;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pedro.nardelli
@@ -7,6 +9,21 @@ package org.codetech.code;
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Conta conta = new Conta();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Agencia: ");
+        int receberNumero = scanner.nextInt();
+
+        if(receberNumero != conta.numero){
+            System.out.println("Agencia não existe, por favor re-valide!!");
+        }else {
+            System.out.println("Olá " + conta.nomeCliente + " obrigado por criar uma conta em nosso banco, sua agencia é " + conta.agencia + " conta " + conta.getNumero() +
+            " e seu saldo " + conta.getSaldo() + " já está dispinivel para saque.");
+        }
+
+        System.out.println("Fim do programa!");
+
     }
 }
